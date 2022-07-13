41e879f815866d34cc042fb405be9e5fe4494129
KceniaB <kcenia.bougrova@research.fchampalimaud.org>
2022-07-13 20:07:09 +0100

    plots optimized but still odd size

--- performance_plot.py
+++ performance_plot.py
@@ -101,4 +101,94 @@ for eid in eids[1:10]:
     #fig.savefig("psychometric_"+ref.subject+str(ref.date)+eid+".png") 
 
 #plot_performance(performance,contrasts,n_contrasts) 
+#%% Plot unsigned 
+
+def unsigning(array): 
+    array = performance
+    a=[]
+    b=[]
+    c=[]
+    d=[]
+    if len(array) == 9: 
+        a = np.mean((array[3],array[5]), dtype=float)
+        b = np.mean((array[2],array[6]), dtype=float)
+        c = np.mean((array[1],array[7]), dtype=float)
+        d = np.mean((array[0],array[8]), dtype=float) 
+    new_array=[]
+    new_array = [array[4],a,b,c,d]
+    return new_array 
+test = unsigning(performance) 
+test_2 = [0.,6.25,12.5,25,100]
+
+def unsigning(array): 
+    array = performance
+    a=[]
+    b=[]
+    c=[]
+    d=[]
+    if len(array) == 9: 
+        a = np.sum((array[3],array[5]), dtype=float)
+        b = np.sum((array[2],array[6]), dtype=float)
+        c = np.sum((array[1],array[7]), dtype=float)
+        d = np.sum((array[0],array[8]), dtype=float) 
+    new_array=[]
+    new_array = [array[4],a,b,c,d]
+    return new_array 
+test_3 = unsigning(n_contrasts)
+# %% 
+def plot_performance_2(performance,contrasts,n_contrasts): 
+    plt.rcParams['figure.figsize'] = [5, 5]
+    plt.rcParams["figure.dpi"] = 100
+
+    #colors 
+    pm = np.mean(performance) 
+    if pm <= 0.5: 
+        c = "#F94144"
+    elif 0.5 <= pm and pm < 0.6: 
+        c = "#F3722C" 
+    elif 0.6 <= pm and pm < 0.7: 
+        c = "#F8961E" 
+    elif 0.7 <= pm and pm < 0.75: 
+        c = "#F9C74F" 
+    elif 0.75 <= pm and pm < 0.85: 
+        c = "#90BE6D"
+    elif 0.85 <= pm and pm < 0.9: 
+        c = "#43AA8B"
+    elif 0.9 < pm: 
+        c = "#577590"
+    else: 
+        c = "black"
+        print("ERROR")
+    
+    
+    contrasts_2 = [0. , 25. , 100. ]
+    plt.plot(contrasts,performance,c=c,alpha=0.25)
+    plt.scatter(contrasts,performance,s=n_contrasts,c=c) 
+    #plt.axhline(y=0.75,color = 'gray', linestyle = '--',linewidth=0.25) 
+    #plt.axvline(x=0,color = 'gray', linestyle = '--',linewidth=0.25) 
+    plt.title(eid)
+    ref = one.eid2ref(eid)
+    #plt.text(50,0.35, ref.subject + '\n' + str(ref.date),  color='#ADB6C4', fontsize=12)
+    #plt.ylim(0.3,1)
+    plt.ylabel('Performance')
+    plt.xlabel('Contrasts') 
+    plt.xticks(contrasts_2)
+    plt.show() 
+    
+    #plt.savefig("performance_"+ref.subject+str(ref.date)+eid+".png") 
+    return plt
+
+# %%
+
+    trials = one.load_object(eid, 'trials', collection='alf')
+    ref = one.eid2ref(eid)
+    performance, contrasts, n_contrasts = compute_performance(trials)
+
+    fig = plot_performance_2(test,test_2,test_3)
+
+
+
+
+
+
 # %%
